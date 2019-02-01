edf5a6f648e871654c93c199869107f6c19a2cae
cvmartin <cvarelamail@gmail.com>
2019-02-01 15:52:41 +0100

    refactored backshift. Early points in the timeseries work

--- src/backshift.cpp
+++ src/backshift.cpp
@@ -66,101 +66,66 @@ List backshiftCpp(arma::vec consumption,
                   List eff,
                   int horizon){
   
-  // how to determine the nrow of the matrix?
-  arma::mat mtx_moves = arma::zeros<arma::mat>(10000,3);
-  // counter for the nrows of the matrix
-  int r = 0;
-  
-  int loc_min = 0;
-  int cons_length = consumption.n_elem;
-  double gain = 0;
-  double piece = (max(consumption)-min(consumption))/100;
+  // size of the piece
+  int precision = 100;
+  double piece = (max(consumption)-min(consumption))/precision;
   
   arma::vec vec_local_empty = arma::zeros<arma::vec>(horizon);
   arma::vec vec_local_cons = arma::zeros<arma::vec>(horizon);
   arma::vec vec_local_diff = arma::zeros<arma::vec>(horizon);
-  
   arma::vec vec_offset = arma::zeros<arma::vec>(horizon);
-  vec_offset.fill(NA_INTEGER);
+  vec_offset.fill(NA_REAL);
+  
+  int cons_length = consumption.n_elem;
+  // Define the size of the matrix
+  arma::mat mtx_moves = arma::zeros<arma::mat>(cons_length*precision,3);
+  // counter for the nrows of the matrix
+  int nrow_moves = 0;
   
   for (int i=0; i < cons_length; ++i) {
-    // start with an afresh copy of consumption
-    arma::vec cons_copy = consumption;
-    // arma::vec cons_copy = vec_offset;
-    // cons_copy.insert_rows(cons_copy.n_rows, consumption);
+    // copy of consumption with a bunch of NA before
+    arma::vec cons_copy = vec_offset;
+    cons_copy.insert_rows(cons_copy.n_rows, consumption);
+    // `io` is `i` plus the offset of NAs that are at the start of cons_copy
+    // To work with the time horizon that is before the initial time boundary. 
+    int io = i + horizon;
     
-    // if (i == 23) {
-    //   Rcout << cons_copy;
-    // }
-    // 
-    while(cons_copy[i] > 0) {
+    while(cons_copy[io] > 0) {
       
       arma::vec vec_local_deprec = depreciate(
-        vec_local_empty.fill(cons_copy[i]),
+        vec_local_empty.fill(cons_copy[io]),
         self_discharge, 
         eff, 
         true
       );
-      // if I wanted to consider only one piece...
-      // deprec = deprec + cons_copy[i] - (piece);
-     
-     int start_from = i-horizon + 1;
-     if (start_from < 0) break; //start_from = 0;
-      
-      vec_local_cons = cons_copy.subvec(start_from, i);
-      
-      // if (start_from == 0) {
-      //   vec_local_deprec = vec_local_deprec.tail(vec_local_cons.n_elem);
-      // }
-      
-      // Rcout << "vec_local_deprec: ";
-      // Rcout << vec_local_deprec;
-      // Rcout << "vec_local_cons: ";
-      // Rcout << vec_local_cons;
-      
+      // local consumption to compare with
+      vec_local_cons = cons_copy.subvec(io - horizon + 1, io);
+      // find the best point comparing the initial consumption
+      // and the depreciation curve 
       vec_local_diff = vec_local_deprec - vec_local_cons;
-      
-      
-      loc_min = vec_local_diff.index_max();
-      
-      // if (i == position){
-      //   Rcout << "deprec";
-      //   Rcout << deprec;
-      //   Rcout << "\n";
-      //   Rcout << "vec_local_diff";
-      //   Rcout << vec_local_diff;
-      //   veccon = vec_local_cons;
-      //   vecdep = deprec;
-      //   Rcout << "loc_min: ";
-      //   Rcout << loc_min;
-      // }
-      
+      int loc_min = vec_local_diff.index_max();
       // if there is no good place to allocate, break
       if (vec_local_diff[loc_min] <= 0) break;
-      
-      cons_copy[i] = cons_copy[i] - piece;
-      
-      int pos_min = i - horizon + loc_min;
-        
-      // if (start_from == 0) {
-      //   pos_min = loc_min;
-      // }
-      
-      gain = (piece * cons_copy[i]) - (piece * cons_copy[pos_min]);
+      // diminish the consumption and repeat
+      cons_copy[io] = cons_copy[io] - piece;
+      // find the position to put the piece in 
+      int pos_min = io - horizon + loc_min;
+      double gain = (piece * cons_copy[io]) - (piece * cons_copy[pos_min]);
       
       // populate the matrix: from
-      mtx_moves(r,0) = i;
-      // to
-      mtx_moves(r,1) = i - horizon + loc_min;
+      mtx_moves(nrow_moves,0) = i;
+      // times back
+      mtx_moves(nrow_moves,1) = loc_min - horizon;
       // gain
-      mtx_moves(r,2) = gain;
-      
-      r = r+1;
-    
+      mtx_moves(nrow_moves,2) = gain;
+      // additional row in the mtx_moves
+      nrow_moves = nrow_moves + 1;
     }
   }
   
-  mtx_moves = mtx_moves.head_rows(r);
+  mtx_moves = mtx_moves.head_rows(nrow_moves);
+  
+  // PART 2: SETUP THE POINTS
   
   return List::create(_["mtx_moves"]= mtx_moves);
 }
